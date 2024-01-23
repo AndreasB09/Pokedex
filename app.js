5391d662c4a531324f0a42db714f78037e596b6e
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-01-23 12:55:16 +0100

    working start

--- app.js
+++ app.js
@@ -2,23 +2,49 @@ const mainContainer = document.getElementById("main-container");
 
 const pokedexUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=3";
 
-let pokemonList = [];
-
 async function displayPokemonList() {
     await updatePokemonList(pokedexUrl);
-    clearMainContainer();
 
     for (const pokemon of pokemonList.results) {
-        const pokemonElement = document.createElement("div");
-        pokemonElement.textContent = pokemon.url;
-        mainContainer.appendChild(pokemonElement);
+        const pokemonDetails = await getPokemonDetails(pokemon.url);
+        displayPokemonDetails(pokemonDetails);
     }
 }
 
-function clearMainContainer() {
-    while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.firstChild);
+async function getPokemonDetails(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching Pokemon details:", error);
+        return null;
     }
+}
+
+function displayPokemonDetails(pokemonData) {
+    if (!pokemonData) return;
+
+    const { id, name, sprites } = pokemonData;
+
+    const containerEl = document.createElement("div");
+    containerEl.classList.add("pokemon-container");
+
+    const numberEl = document.createElement("p");
+    numberEl.textContent = `#${id}`;
+
+    const titleEl = document.createElement("h2");
+    titleEl.textContent = name;
+
+    const imageEl = document.createElement("img");
+    imageEl.alt = `Image of ${name}`;
+    imageEl.src = sprites.other["official-artwork"].front_default;
+
+    containerEl.appendChild(numberEl);
+    containerEl.appendChild(titleEl);
+    containerEl.appendChild(imageEl);
+
+    mainContainer.appendChild(containerEl);
 }
 
 async function updatePokemonList(url) {
