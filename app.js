b5f03b82318898d1701bd379e0719110bf25b0eb
andreasb09 <andreasb09.kodehode@gmail.com>
2024-01-29 14:43:50 +0100

    base

--- app.js
+++ app.js
@@ -4,76 +4,75 @@ const buttonPrev = document.getElementById("btn-prev");
 const buttonNext = document.getElementById("btn-next");
 const buttonRandom = document.getElementById("btn-random");
 
-
 const pokedexUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=3";
 let pokemonList = [];
 
-async function displayPokemonList() {
-    await updatePokemonList(pokedexUrl);
-
-    clearMainCOntainer();
-
-    for (const pokemon of pokemonList.results) {
-        const pokemonDetails = await getPokemonDetails(pokemon.url);
-        displayPokemonDetails(pokemonDetails);
-    }
+function clearMainContainer() {
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild);
+  }
 }
 
-function clearMainCOntainer() {
-    while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.firstChild);
-    }
+async function displayPokemonList() {
+  await updatePokemonList(pokedexUrl);
+
+  clearMainContainer();
+
+  for (const pokemon of pokemonList.results) {
+    const pokemonDetails = await getPokemonDetails(pokemon.url);
+    displayPokemonDetails(pokemonDetails);
+  }
 }
 
 async function getPokemonDetails(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching Pokemon details:", error);
-        return null;
-    }
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching Pokemon details:", error);
+    return null;
+  }
 }
 
 function displayPokemonDetails(pokemonData) {
-    if (!pokemonData) return;
+  if (!pokemonData) return;
 
-    const { id, name, sprites } = pokemonData;
+  const { id, name, sprites } = pokemonData;
 
-    const containerEl = document.createElement("div");
-    containerEl.classList.add("pokemon-container");
+  const containerEl = document.createElement("div");
+  containerEl.classList.add("pokemon-container");
 
-    const numberEl = document.createElement("p");
-    numberEl.textContent = `#${id}`;
+  const numberEl = document.createElement("p");
+  numberEl.textContent = `#${id}`;
 
-    const titleEl = document.createElement("h2");
-    titleEl.textContent = name;
+  const titleEl = document.createElement("h2");
+  titleEl.textContent = name;
 
-    const imageEl = document.createElement("img");
-    imageEl.alt = `Image of ${name}`;
-    imageEl.src = sprites.other["official-artwork"].front_default;
+  const imageEl = document.createElement("img");
+  imageEl.alt = `Image of ${name}`;
+  imageEl.src = sprites.other["official-artwork"].front_default;
 
-    containerEl.appendChild(numberEl);
-    containerEl.appendChild(titleEl);
-    containerEl.appendChild(imageEl);
+  containerEl.appendChild(numberEl);
+  containerEl.appendChild(titleEl);
+  containerEl.appendChild(imageEl);
 
-    mainContainer.appendChild(containerEl);
+  mainContainer.appendChild(containerEl);
 }
 
 async function updatePokemonList(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        pokemonList = data;
-    } catch (error) {
-        console.error("Error fetching Pokemon list:", error);
-    }
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    pokemonList = data;
+  } catch (error) {
+    console.error("Error fetching Pokemon list:", error);
+  }
 }
 
 //button events
 buttonStart.addEventListener("click", () => {
-    displayPokemonList();
+  displayPokemonList();
 });
 
 buttonNext.addEventListener("click", () => {
