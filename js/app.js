570212f9ab4418acb183b57a4e120686fb84331e
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-02-04 13:36:35 +0100

    fixed types

--- js/app.js
+++ js/app.js
@@ -50,6 +50,27 @@ async function displayPokemonList(url) {
 
   mainContainer.innerHTML = ""
 
+  const typeColors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD'
+  }
+
   for (const pokemon of pokemonList.results) {
 
     const pokemonExtraData = await getData(pokemon.url)
@@ -58,14 +79,26 @@ async function displayPokemonList(url) {
     containerEl.classList.add("pokemon-container")
     const titleEl = document.createElement("h2")
     const upperCaseName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    titleEl.textContent = `${pokemonExtraData.id}. ${upperCaseName}`;
+    titleEl.textContent = `${pokemonExtraData.id}. ${upperCaseName}`
 
     const imageEl = document.createElement("img")
     imageEl.alt = `image of ${pokemon.name}`
     imageEl.style = "max-width: 40%;"
     imageEl.src = pokemonExtraData.sprites.other["official-artwork"].front_default
 
-    containerEl.append(titleEl, imageEl)
+    const typeContainer = document.createElement("div")
+    typeContainer.classList.add("type-container")
+
+    pokemonExtraData.types.forEach(typeData => {
+      const typeEl = document.createElement("p")
+      const upperCaseType = typeData.type.name.charAt(0).toUpperCase() + typeData.type.name.slice(1)
+      typeEl.textContent = upperCaseType
+      typeEl.classList.add("type-box")
+      typeEl.style.backgroundColor = typeColors[typeData.type.name] || '#888'
+      typeContainer.append(typeEl)
+    });
+
+    containerEl.append(titleEl, imageEl, typeContainer)
     mainContainer.append(containerEl)
 
     containerEl.addEventListener("click", () => displayPokemonDetails(pokemonExtraData))
